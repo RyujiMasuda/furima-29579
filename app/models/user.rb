c22d516e7c983fbecefb5a5b5f7d0be4ac721d38
RyujiMasuda <ryuji-basketball@i.softbank.jp>
2021-02-07 20:00:42 +0900

    バリデーション 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 6 }
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 
+  validates :birthday, presence: true
+  
 end
