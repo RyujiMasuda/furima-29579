4bb74a188c94fae99b20f868bfd52771cf1c1c5b
RyujiMasuda <ryuji-basketball@i.softbank.jp>
2021-02-11 13:00:45 +0900

    バリデーション 々などを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 6 }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+  with_options presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
     validates :first_name
     validates :last_name
   end
