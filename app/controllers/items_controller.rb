6db0b6aa2f881c811c3dbfc4a3e494fc834e3b78
RyujiMasuda <ryuji-basketball@i.softbank.jp>
2021-02-12 16:26:26 +0900

    コントローラー　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,32 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, 
+      :category_id, 
+      :condition_id, 
+      :text, 
+      :prefecture_id, 
+      :day_id, 
+      :delivery_fee_id, 
+      :price, 
+      :image
+    ).merge(user_id: current_user.id)
   end
 
 end
