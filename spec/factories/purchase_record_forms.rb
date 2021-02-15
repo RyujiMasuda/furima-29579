237ae85eb7837a95f58b6eaa930361bd033af3f3
RyujiMasuda <ryuji-basketball@i.softbank.jp>
2021-02-15 13:15:01 +0900

    単体テストコード

---
+++ spec/factories/purchase_record_forms.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_record_form do
+    prefecture_id {2}
+    municipality {"世田谷区奥沢"}
+    street {"7-40-24"}
+    building {}
+    post {"123-4567"}
+    phone {"08043873210"}
+  end
+end
